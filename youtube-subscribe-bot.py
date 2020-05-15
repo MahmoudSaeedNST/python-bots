f09dd3968fe757b253571bc6a4ea4bb34ed961c9
Mahmoud Saeed <63323010+MahmoudSaeedNST@users.noreply.github.com>
2020-05-15 19:21:44 +0200

    Add files via upload

---
+++ youtube-subscribe-bot.py
@@ -0,0 +1,45 @@
+import time
+from webbot import Browser
+import pyautogui # its use for keyboard inputs.
+import win32console,win32gui
+
+class SubBot():
+    
+    email = []
+    password = []
+    
+    def __init__(self,filename):
+        self.filename = filename
+
+    def readFile(self):
+        with open(self.filename) as fp:
+            for line in fp:
+                file_email,file_passwords = line.split()
+                self.email.append(file_email)
+                self.password.append(file_passwords)
+        
+    def hide(self):
+        window = win32console.GetConsoleWindow()
+        win32gui.ShowWindow(window,0) 
+        
+    def main(self):
+        for i in range(0,len(self.email)):
+            web = Browser('switch_to_alert')
+            self.hide()
+            
+            web.go_to('mail.google.com')
+            web.type(self.email[i] , into='Email')
+            web.click('NEXT' , tag='span')
+            time.sleep(1)
+            web.type(self.password[i], into='Password' , id='passwordFieldId')
+            web.click('NEXT' , tag='span') # you are logged in . woohoooo
+            time.sleep(2)
+            web.go_to('youtube.com/channel/UCSu-YBmsmjSlPTKtlKx_img/')
+            time.sleep(1)
+            web.click('Subscribe',tag='yt-formatted-string')
+            time.sleep(2)
+            pyautogui.hotkey('alt','f4')
+
+sb = SubBot("file.txt")
+sb.readFile()
+sb.main()
